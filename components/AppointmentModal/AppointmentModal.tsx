de903d573c3b16fb2aea1acda124e3ef5efea8cd
chirigutraul <chirigutraul@gmail.com>
2023-03-09 23:09:09 +0200

    started responsive on lg

--- components/AppointmentModal/AppointmentModal.tsx
+++ components/AppointmentModal/AppointmentModal.tsx
@@ -1,20 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Session } from 'next-auth';
-import { montserrat, roboto } from 'utils/fonts';
-import Flatpickr from 'react-flatpickr';
-import ReactModal from 'react-modal';
+import React, { useEffect, useState } from "react";
+import { Session } from "next-auth";
+import { montserrat, roboto } from "utils/fonts";
+import Flatpickr from "react-flatpickr";
+import ReactModal from "react-modal";
 import "flatpickr/dist/themes/airbnb.css";
 import {
   ServicesDropdown,
   Button,
   AvailableHoursDropdown,
-  RequestFeedback
-} from 'components';
-import { Appointment, Service } from '@prisma/client';
-import moment from 'moment';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarPlus, faXmarkCircle } from '@fortawesome/free-regular-svg-icons';
-import { createAppointment } from 'utils/hooks/requests/appointments';
+  RequestFeedback,
+} from "components";
+import { Appointment, Service } from "@prisma/client";
+import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarPlus,
+  faXmarkCircle,
+} from "@fortawesome/free-regular-svg-icons";
+import { createAppointment } from "utils/hooks/requests/appointments";
+import breakpoints from "utils/TailwindBreakPoints";
+import useWindowSize from "utils/hooks/BreakPointsHooks";
 
 interface Props {
   isOpen: boolean;
@@ -24,50 +29,61 @@ interface Props {
 
 const AppointmentModal = ({ session, isOpen, toggleModal }: Props) => {
   const [date, setDate] = useState<Date>();
-  const [hour, setHour] = useState<string>('');
+  const [hour, setHour] = useState<string>("");
   const [appointments, setAppointments] = useState<Appointment[]>([]);
   const [services, setServices] = useState<Service[]>([]);
   const [selectedService, setSelectedService] = useState<Service>();
   const [response, setResponse] = useState<{
-    status: number,
-    message: string,
-  }>()
+    status: number;
+    message: string;
+  }>();
+
+  const { width } = useWindowSize();
+  const isScreenAboveMedium = width && width >= breakpoints.md;
+  const isScreenAboveLarge = width && width >= breakpoints.lg;
 
   const fetchAppointments = async () => {
-    const response = await fetch('/api/appointments/get-appointments-from-date', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ date: moment(date).format() })
-    });
+    const response = await fetch(
+      "/api/appointments/get-appointments-from-date",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ date: moment(date).format() }),
+      }
+    );
     const data = await response.json();
     setAppointments(data);
-  }
+  };
 
   const fetchServices = async () => {
-    const response = await fetch('/api/services');
+    const response = await fetch("/api/services");
     const data = await response.json();
     setServices(data);
-  }
+  };
 
   const handleAppointmentCreation = async () => {
     if (!date || !hour || !selectedService) return;
 
-    const response = await createAppointment(session.user.id, date, hour, selectedService)
-      .then(async res => {
-        const parsedResponse = await res.json();
-        setResponse(parsedResponse);
-      })
+    const response = await createAppointment(
+      session.user.id,
+      date,
+      hour,
+      selectedService
+    ).then(async (res) => {
+      const parsedResponse = await res.json();
+      setResponse(parsedResponse);
+    });
 
-    console.log('response', response)
+    console.log("response", response);
     // toggleModal();
-  }
+  };
 
   useEffect(() => {
-    fetchAppointments()
+    fetchAppointments();
     fetchServices();
-  }, [date])
+  }, [date]);
 
   return (
     <ReactModal
@@ -76,85 +92,102 @@ const AppointmentModal = ({ session, isOpen, toggleModal }: Props) => {
       className={`
       py-8 px-4
       w-full h-screen overflow-scroll absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] 
-    bg-primary rounded-md shadow-md outline-none
+      bg-primary rounded-md shadow-md outline-none
+      sm:px-8
+      md:h-[80vh]
+      md:bg-red-300
     `}
       overlayClassName={`h-screen w-screen absolute top-0 bg-[rgba(0,0,0,0.5)] border-0 backdrop-blur`}
     >
-      <form className={`
+      <form
+        className={`
        h-full w-full flex flex-col items-center justify-between
-      `}>
-        <div className={'h-full w-full flex flex-col items-center gap-8'}>
-          <div className={`
-        flex flex-row justify-between w-full items-center
-        `}>
-            <h1 className={`
-         ${montserrat.className}
-        text-3xl font-light 
-        sm:text-4xl
-        `}>
+      `}
+      >
+        <div className={"h-full w-full flex flex-col items-center gap-8"}>
+          <div className={`flex flex-row justify-between w-full items-center`}>
+            <h1
+              className={`
+              ${montserrat.className}
+              text-3xl font-light 
+              sm:text-4xl
+              md:text-3xl md:text-center
+              `}
+            >
               Buna, {session?.user.name}
             </h1>
-            <div
-              onClick={toggleModal}
-            >
-              <FontAwesomeIcon
-                icon={faXmarkCircle}
-                className={`text-accent group-hover:text-primary`}
-                size="2xl"
-              />
+            <div onClick={toggleModal}>
+              {!!isScreenAboveLarge && (
+                <FontAwesomeIcon
+                  icon={faXmarkCircle}
+                  className={`text-accent group-hover:text-primary 
+              text-4xl
+              `}
+                />
+              )}
             </div>
           </div>
-          <p className={`
+          <p
+            className={`
           ${roboto.className}
           font-light text-left 
           text-xl
           sm:text-2xl
-        `}>
-            Pentru a realiza o programare, te rugam sa completezi formularul de mai jos.
+        `}
+          >
+            Pentru a realiza o programare, te rugam sa completezi formularul de
+            mai jos.
           </p>
-          <div className={'flex flex-col gap-2 w-full'}>
-            <label className={`
+          <div className={"flex flex-col gap-2 w-full"}>
+            <label
+              className={`
           text-xl ${roboto.className}
           font-light my-0
           sm:text-2xl
-          `}>
+          `}
+            >
               Selecteaza data :
             </label>
             <div
-              className={'w-full flex flex-row bg-white items-center px-4 rounded-sm overflow-hidden shadow-md'}>
+              className={
+                "w-full flex flex-row bg-white items-center px-4 rounded-sm overflow-hidden shadow-md"
+              }
+            >
               <Flatpickr
                 name="date"
                 value={date}
                 className={`
                 w-full py-2 text-left text-xl focus:border-0 focus:outline-none
+                sm:text-xl
                 `}
                 placeholder="Data"
-                onChange={date => setDate(date[0])}
+                onChange={(date) => setDate(date[0])}
                 options={{
                   enableTime: false,
                   time_24hr: true,
                   minDate: "today",
                   disable: [
                     function (date) {
-                      return date.getDay() === 0
+                      return date.getDay() === 0;
                     },
-                  ]
+                  ],
                 }}
               />
               <FontAwesomeIcon
                 icon={faCalendarPlus}
-                className={`text-accent group-hover:text-primary`}
-                size="xl"
+                className={`text-accent group-hover:text-primary text-3xl`}
               />
             </div>
           </div>
 
-          <div className={'flex flex-col gap-2 w-full'}>
-            <label className={`
+          <div className={"flex flex-col gap-2 w-full"}>
+            <label
+              className={`
           text-xl ${roboto.className}
           font-light
           sm:text-2xl
-          `}>
+          `}
+            >
               Selecteaza serviciul dorit :
             </label>
             <ServicesDropdown
@@ -162,14 +195,15 @@ const AppointmentModal = ({ session, isOpen, toggleModal }: Props) => {
               onSelect={setSelectedService}
             />
           </div>
-          {
-            date && selectedService &&
-            <div className={'w-full flex flex-col gap-2'}>
-              <label className={`
+          {date && selectedService && (
+            <div className={"w-full flex flex-col gap-2"}>
+              <label
+                className={`
               text-xl ${roboto.className}
               font-light
               sm:text-2xl
-              `}>
+              `}
+              >
                 Selecteaza una din orele disponibile :
               </label>
               <AvailableHoursDropdown
@@ -179,32 +213,30 @@ const AppointmentModal = ({ session, isOpen, toggleModal }: Props) => {
                 onSelect={setHour}
               />
             </div>
-          }
-          {
-            response &&
+          )}
+          {response && (
             <RequestFeedback
               message={response.message}
               status={response.status}
             />
-          }
+          )}
         </div>
         {
           <Button
             title={
-              response && response.status === 200
-                ? "Inchide"
-                : "Programeaza!"
+              response && response.status === 200 ? "Inchide" : "Programeaza!"
             }
             onClick={
               response && response.status === 200
                 ? toggleModal
                 : handleAppointmentCreation
             }
+            size={isScreenAboveMedium ? "xl" : "lg"}
           />
         }
       </form>
     </ReactModal>
-  )
-}
+  );
+};
 
-export default AppointmentModal
+export default AppointmentModal;
--- components/ServicesDropdown/ServicesDropdown.tsx
+++ components/ServicesDropdown/ServicesDropdown.tsx
@@ -1,7 +1,9 @@
-import { Service, Servname } from '@prisma/client';
-import { FunctionComponent, useState } from 'react';
-import { montserrat, roboto } from 'utils/fonts';
-import { servicesLabels } from 'utils/constants';
+import { Service, Servname } from "@prisma/client";
+import { FunctionComponent, useState } from "react";
+import { montserrat, roboto } from "utils/fonts";
+import { servicesLabels } from "utils/constants";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface DropdownProps {
   options: Service[];
@@ -12,59 +14,71 @@ const Dropdown: FunctionComponent<DropdownProps> = ({ options, onSelect }) => {
   const [selectedValue, setSelectedValue] = useState<Service>();
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => setIsOpen(curr => !curr);
+  const toggleDropdown = () => setIsOpen((curr) => !curr);
 
   return (
     <div className="relative inline-block w-full text-left">
-      <div className='w-full'>
+      <div className="w-full">
         <button
           type="button"
           className={`
-          inline-flex justify-between w-full px-4 py-2 bg-white rounded-sm shadow-md hover:bg-gray-100 focus:outline-none
+          inline-flex justify-between items-center w-full px-4 py-2 bg-white rounded-sm shadow-md hover:bg-gray-100 focus:outline-none
           ${roboto.className} font-regular text-md text-center truncate 
-          ${selectedValue ? 'text-accent' : 'text-gray-400'}
+          ${selectedValue ? "text-accent" : "text-gray-400"}
           `}
-          onClick={() => toggleDropdown()}>
-          <span className={'truncate'}>{(selectedValue && servicesLabels.get(selectedValue.name)) ?? 'Serviciul'}</span>
-          <svg
-            className="w-5 h-5 ml-2 -mr-1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true">
-            <path
-              fillRule="evenodd"
-              d="M10.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 12.586l3.293-3.293a1 1 0 011.414 1.414l-4 4z"
-              clipRule="evenodd"
-            />
-          </svg>
+          onClick={() => toggleDropdown()}
+        >
+          <span
+            className={`
+          truncate
+          sm:text-xl ${roboto.className} font-light
+          `}
+          >
+            {(selectedValue && servicesLabels.get(selectedValue.name)) ??
+              "Serviciul"}
+          </span>
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            className={`
+            text-xl text-accent
+            sm:text-2xl
+            `}
+          />
         </button>
       </div>
 
-      {
-        isOpen &&
+      {isOpen && (
         <div className="absolute right-0 z-10 w-full mt-2 origin-top-right bg-white rounded-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
             {options.map((option) => (
               <button
-                type={'button'}
+                type={"button"}
                 key={option.id}
                 onClick={() => {
                   setSelectedValue(option);
                   onSelect(option);
                   toggleDropdown();
                 }}
-                className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                role="menuitem">
+                className={`
+                block w-full px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900
+                ${roboto.className} font-light text-accent 
+                sm:text-lg sm:py-2
+                `}
+                role="menuitem"
+              >
                 {servicesLabels.get(option.name)}
               </button>
             ))}
           </div>
         </div>
-      }
-
+      )}
     </div>
   );
-}
+};
 
 export default Dropdown;
