1849058487fb59136afd4df78947b2d58bf8d763
chirigutraul <chirigutraul@gmail.com>
2023-03-12 19:20:23 +0200

    fix scroll x appointment history

--- components/AppointmentsHistory/AppointmentsHistory.tsx
+++ components/AppointmentsHistory/AppointmentsHistory.tsx
@@ -1,29 +1,33 @@
-import {AppointmentCard} from 'components'
-import React from 'react'
-import { roboto } from 'utils/fonts'
+import { AppointmentCard } from "components";
+import React from "react";
+import { roboto } from "utils/fonts";
 
 const AppointmentsHistory = () => {
   return (
-    <div className={`
+    <div
+      className={`
     bg-primary px-10 flex flex-col gap-6 py-8
-    md:h-[18rem] md:overflow-scroll md:row-start-2
+    md:h-[18rem] md:overflow-scroll md:row-start-2 md:overflow-x-hidden
     md:px-4 md:box-content
-    `}>
-      <p className={`
+    `}
+    >
+      <p
+        className={`
       ${roboto.className}
       font-light text-center 
       text-2xl
       sm:text-3xl
-      `}>
+      `}
+      >
         Istoricul programarilor tale:
       </p>
-      
-      <AppointmentCard/>
-      <AppointmentCard/>
-      <AppointmentCard/>
-      <AppointmentCard/>
-    </div>
-  )
-}
 
-export default AppointmentsHistory
+      <AppointmentCard />
+      <AppointmentCard />
+      <AppointmentCard />
+      <AppointmentCard />
+    </div>
+  );
+};
+
+export default AppointmentsHistory;
