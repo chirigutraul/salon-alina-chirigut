16254a70f80a4ffe68c97b684471ccfe8049d6ff
chirigutraul <chirigutraul@gmail.com>
2023-04-09 20:46:18 +0300

    added despre page

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,16 +1,25 @@
-import React, { FunctionComponent } from 'react'
-import { roboto } from 'utils/fonts'
+import React, { FunctionComponent } from "react";
+import { roboto } from "utils/fonts";
 
 interface ButtonProps {
-  title: string,
-  variant?: string,
-  onClick(): void,
-  size?: string,
-  type?: "button" | "submit",
-  disabled?: boolean,
+  title: string;
+  variant?: string;
+  onClick(): void;
+  size?: string;
+  type?: "button" | "submit";
+  disabled?: boolean;
+  full?: boolean;
 }
 
-const Button: FunctionComponent<ButtonProps> = ({ title, variant, onClick, size, type, disabled }) => {
+const Button: FunctionComponent<ButtonProps> = ({
+  title,
+  variant,
+  onClick,
+  size,
+  type,
+  disabled,
+  full,
+}) => {
   return (
     <button
       disabled={disabled ?? false}
@@ -20,20 +29,23 @@ const Button: FunctionComponent<ButtonProps> = ({ title, variant, onClick, size,
       px-6 py-2 rounded-sm w-48
       texl-md
       text-white bg-accent
-      ${size === 'xl' && 'px-8 py-3 text-xl w-56'}
-      ${size === '2xl' && 'px-10 py-5 text-2xl w-64'}
-      ${variant === 'light' && 'bg-primary text-accent'}
-      ${disabled ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : ''}
+      ${size === "xl" && "px-8 py-3 text-xl w-56"}
+      ${size === "2xl" && "px-10 py-5 text-2xl w-64"}
+      ${variant === "light" && "bg-primary text-accent"}
+      ${disabled ? "bg-gray-300 text-gray-500 cursor-not-allowed" : ""}
+      ${full ? "w-full" : ""}
     `}
     >
-      <p className={`
-    ${roboto.className}
-    font-light text-center
-    `}>
+      <p
+        className={`
+        ${roboto.className}
+        font-light text-center
+      `}
+      >
         {title}
       </p>
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- pages/despre.tsx
+++ pages/despre.tsx
@@ -1,3 +1,65 @@
+import { Button } from "components";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { roboto } from "utils/fonts";
+
 export default function Despre() {
-  return <div className={"text-white text-2xl p-4"}></div>;
+  const router = useRouter();
+
+  return (
+    <div className={""}>
+      <div
+        className={`text-white relative h-72 w-full bg-black p-4
+      md:py-8 md:px-4
+      lg:py-16 lg:px-32
+      `}
+      >
+        <h1 className={`${roboto.className} text-3xl`}>Despre noi</h1>
+        <h3 className={` text-xl w-[60%] mt-2`}>
+          Manichiura & pedichiura realizate cu drag
+        </h3>
+        <Image
+          src="https://images.unsplash.com/photo-1632345031435-8727f6897d53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          fill
+          alt="Poza cu manichiurista noastra executand o lucrare"
+          className={"opacity-60 object-cover"}
+        />
+      </div>
+      <div
+        className={`
+      flex flex-col gap-4 p-4 text-accent
+      bg-gradient-to-t  from-primary to-white
+      md:py-8 md:px-4
+      lg:py-16 lg:px-32
+      `}
+      >
+        <h3 className={`${roboto.className} text-3xl font-bold`}>
+          Bun venit la Ally Nails
+        </h3>
+        <p className="text-lg font-bold lg:w-[80%] xl:w-[60%]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+          elementum, libero vitae venenatis hendrerit, orci quam mattis nibh, id
+          sagittis enim ante at ex. Curabitur sit amet ligula congue, hendrerit
+          ipsum quis, ultrices turpis. Vivamus pharetra commodo libero.
+        </p>
+        <p className="text-lg font-bold lg:w-[80%] xl:w-[60%]">
+          Aliquam ac tellus a metus hendrerit egestas eget in dui. Proin in
+          convallis arcu, a dictum turpis. Curabitur elementum pretium arcu sed
+          condimentum. Morbi nibh dui, tempus eget nulla at, tempus ultricies
+          purus.
+        </p>
+        <button
+          onClick={() => router.push("/profile")}
+          className={`bg-accent text-white p-4 rounded-md 
+          my-4 text-2xl ${roboto.className} font-thin
+          hover:opacity-90
+          md:w-64
+          lg:my-8
+          `}
+        >
+          Programeaza-te
+        </button>
+      </div>
+    </div>
+  );
 }
