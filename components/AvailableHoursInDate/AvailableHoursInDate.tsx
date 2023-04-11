644672b283e70fe5201cf73b6f0e3c628a4d8da6
chirigutraul <chirigutraul@gmail.com>
2023-04-11 19:17:07 +0300

    revert package json build command changes, fixed hook import issue in AvailableHoursInDate

--- components/AvailableHoursInDate/AvailableHoursInDate.tsx
+++ components/AvailableHoursInDate/AvailableHoursInDate.tsx
@@ -1,6 +1,6 @@
-import { Appointment } from '@prisma/client';
-import React, { FunctionComponent } from 'react'
-import { getAvailableHours } from 'utils/hooks/date/GetAvailableHoursFromDate'
+import { Appointment } from "@prisma/client";
+import React, { FunctionComponent } from "react";
+import { useGetAvailableHours } from "utils/hooks/date/GetAvailableHoursFromDate";
 
 interface Props {
   appointments: Appointment[];
@@ -10,32 +10,44 @@ interface Props {
   selectedServiceDuration: string | null;
 }
 
-const AvailableHoursInDate: FunctionComponent<Props> = ({ appointments, selectedDate, setHour, selectedHour, selectedServiceDuration }) => {
+const AvailableHoursInDate: FunctionComponent<Props> = ({
+  appointments,
+  selectedDate,
+  setHour,
+  selectedHour,
+  selectedServiceDuration,
+}) => {
+  const availableHours = useGetAvailableHours(
+    appointments,
+    selectedDate,
+    selectedServiceDuration
+  );
 
   if (!selectedDate || !selectedServiceDuration) return null;
-
-  const availableHours = getAvailableHours(appointments, selectedDate.toLocaleDateString(), parseInt(selectedServiceDuration));
 
   return (
     <div
       className={`  
     flex flex-row flex-wrap justify-center gap-4 transition-all duration-300
-    `}>
-      {
-        availableHours && availableHours.map((hour, index) =>
-          <div key={index + hour}
+    `}
+    >
+      {availableHours &&
+        availableHours.map((hour, index) => (
+          <div
+            key={index + hour}
             onClick={() => setHour(hour)}
             className={`
         border-2 py-2 px-4 rounded-md cursor-pointer border-accent
         transition-all duration-300
         hover:bg-accent hover:text-white
-        ${selectedHour === hour && 'bg-accent text-white'}
-        `}>
+        ${selectedHour === hour && "bg-accent text-white"}
+        `}
+          >
             {hour}
           </div>
-        )}
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default AvailableHoursInDate
+export default AvailableHoursInDate;
