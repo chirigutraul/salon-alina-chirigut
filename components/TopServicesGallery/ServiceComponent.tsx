4ebdec4b8a006186f3f413d50c1a25227028c1b7
chirigutraul <chirigutraul@gmail.com>
2023-04-13 16:06:38 +0300

    fix top services headings and positioning

--- components/TopServicesGallery/ServiceComponent.tsx
+++ components/TopServicesGallery/ServiceComponent.tsx
@@ -1,60 +1,82 @@
-import React from 'react'
-import { IServicesObject } from 'types'
-import Image from 'next/image'
+import React from "react";
+import { IServicesObject } from "types";
+import Image from "next/image";
 
-import {motion} from 'framer-motion'
-import useWindowSize from 'utils/hooks/BreakPointsHooks'
-import breakpoints from 'utils/TailwindBreakPoints'
+import { motion } from "framer-motion";
+import useWindowSize from "utils/hooks/BreakPointsHooks";
+import breakpoints from "utils/TailwindBreakPoints";
+import { roboto } from "utils/fonts";
 
-const isNumberOdd = (verifiedNumber:number) => verifiedNumber%2===0
+const isNumberOdd = (verifiedNumber: number) => verifiedNumber % 2 === 0;
 
 function ServiceComponent({
   service,
   indexOfItem,
-}:{
-  service:IServicesObject;
-  indexOfItem:number;
+}: {
+  service: IServicesObject;
+  indexOfItem: number;
 }) {
-  const { width } = useWindowSize()
-  const isScreenMedium = (width:number) => width >= breakpoints.md
+  const { width } = useWindowSize();
+  const isScreenMedium = (width: number) => width >= breakpoints.md;
 
-  const animationForSmallScreen = {x: isNumberOdd(indexOfItem) ? '100%' : '-100%'}
-  const animationForMediumScreen = {y: isNumberOdd(indexOfItem) ? '100%' : '-100%'}
+  const animationForSmallScreen = {
+    x: isNumberOdd(indexOfItem) ? "100%" : "-100%",
+  };
+  const animationForMediumScreen = {
+    y: isNumberOdd(indexOfItem) ? "100%" : "-100%",
+  };
 
-  const whileInViewForSmallScreen = {x: '0', y:'0'}
-  const whileInViewForMediumScreen = {y: '0', x:'0'}
-  
-  const initialAnimationVariation= (width && isScreenMedium(width))  ? animationForMediumScreen  : animationForSmallScreen
-  const whileInViewVariation = (width && isScreenMedium(width)) ? whileInViewForMediumScreen : whileInViewForSmallScreen
+  const whileInViewForSmallScreen = { x: "0", y: "0" };
+  const whileInViewForMediumScreen = { y: "0", x: "0" };
+
+  const initialAnimationVariation =
+    width && isScreenMedium(width)
+      ? animationForMediumScreen
+      : animationForSmallScreen;
+  const whileInViewVariation =
+    width && isScreenMedium(width)
+      ? whileInViewForMediumScreen
+      : whileInViewForSmallScreen;
 
   return (
     <motion.div
-    animate={initialAnimationVariation}
-    whileInView={whileInViewVariation}
-    viewport={{once:true}}
-    transition={{duration:1}}
+      animate={initialAnimationVariation}
+      whileInView={whileInViewVariation}
+      viewport={{ once: true }}
+      transition={{ duration: 1 }}
     >
-      <div className='relative h-96
-      lg:h-[32rem]'>
+      <div
+        className={`relative w-full aspect-square
+      md:w-[28vw]
+      `}
+      >
         <Image
-        src={service.pictureOfService}
-        fill={true}
-        className="object-cover"
-        alt={service.nameOfService}
-        sizes='(max-width:1920px) theme(w-full)'
+          src={service.pictureOfService}
+          fill={true}
+          className="object-cover"
+          alt={service.nameOfService}
+          sizes="(max-width:1920px) theme(w-full)"
         />
-      </div>  
-     
-      <div className='text-center'>
-        <p className='text-3xl text-white mt-8'>
+      </div>
+
+      <div
+        className={`w-full mt-4 text-center
+      md:w-[28vw]
+      `}
+      >
+        <p className={`text-2xl text-white ${roboto.className}`}>
           {service.nameOfService}
         </p>
-        <p className='text-1xl text-white mt-8 px-8'>
+        <p
+          className={`px-8 mt-2 text-white text-md 
+          md:px-0
+        `}
+        >
           {service.descriptionOfService}
         </p>
       </div>
     </motion.div>
-  )
+  );
 }
 
-export default ServiceComponent
+export default ServiceComponent;
--- components/TopServicesGallery/TopServicesGallery.tsx
+++ components/TopServicesGallery/TopServicesGallery.tsx
@@ -1,24 +1,27 @@
-import { AnimatePresence } from 'framer-motion';
-import React from 'react'
-import ServiceComponent from './ServiceComponent';
-import servicesArray from './servicesArray';
+import { AnimatePresence } from "framer-motion";
+import React from "react";
+import ServiceComponent from "./ServiceComponent";
+import servicesArray from "./servicesArray";
 
 function TopServicesGallery() {
-
   return (
-    <div className='grid justify-center grid-cols-1 gap-8 px-8 py-8 text-center bg-accent md:grid-cols-2 md:px-8 lg:grid-cols-3 lg:py-16 lg:h-screen lg:items-center'>
+    <div
+      className={`bg-accent flex flex-col
+    items-center p-8 gap-8
+    md:flex-row md:items-start md:justify-center
+    `}
+    >
       <AnimatePresence>
-        { 
-        servicesArray.map((service, index:number) => 
-        <ServiceComponent
-        key={service.nameOfService}
-        service={service}
-        indexOfItem={index}
-        />
-        )}
+        {servicesArray.map((service, index: number) => (
+          <ServiceComponent
+            key={service.nameOfService}
+            service={service}
+            indexOfItem={index}
+          />
+        ))}
       </AnimatePresence>
     </div>
-  )
+  );
 }
 
-export default TopServicesGallery
+export default TopServicesGallery;
