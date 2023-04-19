efedefb9b2ee05de79ccdb1755c74e43e966fda3
chirigutraul <chirigutraul@gmail.com>
2023-04-19 15:13:19 +0300

    removed unused swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/layout";
-import { SWRConfig } from "swr";
 import { SessionProvider } from "next-auth/react";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
@@ -9,14 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Layout>
-        <SWRConfig
-          value={{
-            refreshInterval: 3000,
-            fetcher: (url: string) => fetch(url).then((res) => res.json()),
-          }}
-        >
-          <Component {...pageProps} />
-        </SWRConfig>
+        <Component {...pageProps} />
       </Layout>
     </SessionProvider>
   );
