473b900429075dd2139179aa5e74a05f899b6988
chirigutraul <chirigutraul@gmail.com>
2023-04-20 12:28:46 +0300

    finished navbar styling

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import useWindowSize from "utils/hooks/BreakPointsHooks";
 import breakpoints from "utils/TailwindBreakPoints";
@@ -7,9 +7,11 @@ import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faSignIn } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faClose, faSignIn } from "@fortawesome/free-solid-svg-icons";
 import { links } from "./navbarLinks";
 import ReactModal from "react-modal";
+import Link from "next/link";
+
 const Navbar = () => {
   const router = useRouter();
   const { width } = useWindowSize();
@@ -22,84 +24,137 @@ const Navbar = () => {
 
   const navigateToSignIn = () => router.push("/profile");
 
-  const DesktopLinks = () => (
-    <div className={"hidden md:flex md:gap-8 items-center gap-24"}>
-      <ul className={"flex gap-8 text-black"}>
-        {links.map((link) => (
-          <li>
-            <a href={link.route}>{link.title}</a>
-          </li>
-        ))}
-      </ul>
-      <div>
-        <button
-          className={"btn btn-gradient btn-icon"}
-          onClick={navigateToSignIn}
+  const navigateHome = () => router.push("/");
+
+  if (isHamburgerOpen)
+    return (
+      <HamburgerMenu
+        isHamburgerOpen={isHamburgerOpen}
+        toggleNavbar={toggleNavbar}
+        navigateToSignIn={navigateToSignIn}
+      />
+    );
+
+  return (
+    <>
+      <nav
+        className={`
+      bg-white-80 left-0 right-0 absolute z-50 flex h-[96px] xl:h-[128px] justify-between items-center px-4 border-black-10 border-b-2
+      ${isHamburgerOpen ? "" : ""}
+      xl:px-16 
+      `}
+      >
+        <div
+          onClick={navigateHome}
+          className={`relative h-[48px] w-[147px] md:h-[55px] md:w-[172px] xl:h-[87px] xl:w-[268px] cursor-pointer`}
         >
-          <h6>Conectare</h6>
-          <FontAwesomeIcon
-            icon={faSignIn}
-            className={`
-        text-xl
-        md:text-2xl
-        `}
-          />
-        </button>
-      </div>
-    </div>
+          <Image src="/images/logo-desktop.png" alt="Logo-ul salonului" fill />
+        </div>
+        {isLarge ? (
+          <DesktopLinks navigateToSignIn={navigateToSignIn} />
+        ) : (
+          <HamburgerButton toggleNavbar={toggleNavbar} />
+        )}
+      </nav>
+    </>
   );
+};
 
-  const HamburgerButton = () => (
-    <div
-      className={
-        "grid place-items-center bg-gradient h-16 w-16 text-white rounded-md btn-gradient"
-      }
-      onClick={toggleNavbar}
-    >
-      <FontAwesomeIcon icon={faBars} className={`text-2xl`} />
+interface DesktopLinksProps {
+  navigateToSignIn: () => void;
+}
+
+const DesktopLinks: FunctionComponent<DesktopLinksProps> = ({
+  navigateToSignIn,
+}) => (
+  <div className={"hidden md:flex md:gap-8 items-center gap-24"}>
+    <ul className={"flex gap-8 text-black"}>
+      {links.map((link) => (
+        <li>
+          <Link href={link.route}>{link.title}</Link>
+        </li>
+      ))}
+    </ul>
+    <div>
+      <button
+        className={"btn btn-gradient btn-icon"}
+        onClick={() => navigateToSignIn()}
+      >
+        <h6>Conectare</h6>
+        <FontAwesomeIcon icon={faSignIn} className={`text-xl md:text-2xl`} />
+      </button>
     </div>
-  );
+  </div>
+);
 
-  const HamburgerMenu = () => (
+interface HamburgerButtonProps {
+  toggleNavbar: () => void;
+}
+
+const HamburgerButton: FunctionComponent<HamburgerButtonProps> = ({
+  toggleNavbar,
+}) => (
+  <div
+    className={
+      "grid place-items-center bg-gradient p-4 text-white rounded-md btn-gradient cursor-pointer"
+    }
+    onClick={toggleNavbar}
+  >
+    <FontAwesomeIcon icon={faBars} className={`text-2xl`} />
+  </div>
+);
+
+interface HamburgerMenuProps {
+  isHamburgerOpen: boolean;
+  toggleNavbar: () => void;
+  navigateToSignIn: () => void;
+}
+
+const HamburgerMenu: FunctionComponent<HamburgerMenuProps> = ({
+  navigateToSignIn,
+  isHamburgerOpen,
+  toggleNavbar,
+}) => {
+  const navigateAndClose = () => {
+    navigateToSignIn();
+    toggleNavbar();
+  };
+
+  return (
     <ReactModal
       isOpen={isHamburgerOpen}
       onRequestClose={toggleNavbar}
       className={`
-      w-[80%] z-50 bg-gradient h-screen py-8 px-4 overflow-hidden
-      `}
+    w-[80%] z-50 bg-gradient h-screen py-8 px-8  text-white 
+    `}
     >
-      <nav>
-        <ul className={"flex flex-col gap-8 text-white"}>
+      <nav className={"relative h-full w-full"}>
+        <button onClick={toggleNavbar}>
+          <FontAwesomeIcon
+            icon={faClose}
+            className={`absolute right-8 text-4xl`}
+          />
+        </button>
+        <ul className={"flex flex-col gap-16"}>
           {links.map((link) => (
-            <li>
-              <a href={link.route}>
+            <li onClick={toggleNavbar}>
+              <Link href={link.route}>
                 <h5>{link.title}</h5>
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
+        <div className={"absolute bottom-0"}>
+          <button
+            onClick={navigateAndClose}
+            className={"btn btn-border-light btn-icon"}
+          >
+            <h5>Conectare</h5>
+            <FontAwesomeIcon icon={faSignIn} />
+          </button>
+        </div>
       </nav>
     </ReactModal>
-  );
-
-  return (
-    <nav
-      className={`
-      bg-white-80 left-0 right-0 absolute flex h-[96px] xl:h-[128px] justify-between items-center px-4 border-black-10 border-b-2
-      ${isHamburgerOpen ? "" : "z-10"}
-      xl:px-16 
-      `}
-    >
-      <div
-        className={`relative h-[48px] w-[147px]
-      md:h-[55px] md:w-[172px] xl:h-[87px] xl:w-[268px]
-      `}
-      >
-        <Image src="/images/logo-desktop.png" alt="Logo-ul salonului" fill />
-      </div>
-      {isLarge ? <DesktopLinks /> : <HamburgerButton />}
-      {isHamburgerOpen && <HamburgerMenu />}
-    </nav>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/layout";
 import { SessionProvider } from "next-auth/react";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
