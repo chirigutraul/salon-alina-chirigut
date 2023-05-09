89735f8f30e73cfa156203a9db8b05b531941629
chirigutraul <chirigutraul@gmail.com>
2023-05-09 11:11:25 +0300

    removed unused icons, added google badge

--- components/UnauthenticatedUser/unauthenticated-user.tsx
+++ components/UnauthenticatedUser/unauthenticated-user.tsx
@@ -8,17 +8,30 @@ const UnauthenticatedUser = () => {
     signIn("google", {});
   };
   return (
-    <section className={`bg-gradient sp-h`}>
+    <section className={`bg-gradient sp-h grid place-items-center`}>
       <div
-        className={`text-white sp-4t sp-2h sp-2v flex flex-col items-center text-center bg-black-75 w-full justify-between rounded-lg`}
+        className={`text-white sp-4t sp-2h sp-2v flex flex-col items-center text-center bg-black-75 w-full justify-between rounded-lg
+        max-w-[500px]
+        xl:sp-h
+        `}
       >
-        <h3>
+        <h5>
           Pentru a putea face programari, te rugam sa te autentifici folosind
           una din metodele de mai jos:
-        </h3>
+        </h5>
         <div className={`sp-2t`}>
-          <div className={`w-72 h-16 bg-white rounded-md`}></div>
-          <div className={`w-72 h-16 bg-white sp-t rounded-md`}></div>
+          <Image
+            src="/images/sign-in-badges/google-2x.png"
+            width={382}
+            height={92}
+            alt="Sign in with google button"
+            className={`scale-75`}
+          />
+          <div
+            className={`w-72 h-16 bg-white sp-t rounded-md
+          xl:w-96
+          `}
+          ></div>
         </div>
       </div>
     </section>
