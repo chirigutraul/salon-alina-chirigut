28face92ff81fc54b5cf359f762f789ccd3672dc
chirigutraul <chirigutraul@gmail.com>
2023-06-11 22:05:15 +0300

    added react toastify

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -2,12 +2,17 @@ import React, { ReactElement } from "react";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function Layout({ children }: { children: ReactElement }) {
   return (
     <div>
       <Navbar />
       {children}
       <Footer />
+
+      <ToastContainer theme="dark" position="bottom-right" />
     </div>
   );
 }
