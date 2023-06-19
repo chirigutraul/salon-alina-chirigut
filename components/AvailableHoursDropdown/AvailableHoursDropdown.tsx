c962f513ec432d8df0592f65d2ac506ea29a12d8
chirigutraul <chirigutraul@gmail.com>
2023-06-19 08:42:24 +0300

    push for debug

--- components/AvailableHoursDropdown/AvailableHoursDropdown.tsx
+++ components/AvailableHoursDropdown/AvailableHoursDropdown.tsx
@@ -34,6 +34,7 @@ const AvailableHoursDropdown: FunctionComponent<DropdownProps> = ({
   return (
     <div>
       <Label text="Ora" />
+      {availableHours}
       <div
         className={`relative rounded-md h-12 text-black bg-black-25 cursor-pointer font-light`}
       >
