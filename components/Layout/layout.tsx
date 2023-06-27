c5a3f14dbc87fb71a997ee804bb138e2e3887add
chirigutraul <chirigutraul@gmail.com>
2023-06-27 22:30:12 +0300

    added test loader

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,10 +1,11 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, Suspense } from "react";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/router";
+import Loading from "./loading";
 
 const shouldBackgroundBeGradient = {
   "/profile": true,
@@ -15,20 +16,20 @@ export default function Layout({ children }: { children: ReactElement }) {
   const path = router.asPath as keyof typeof shouldBackgroundBeGradient;
 
   return (
-    <div
-      className={`
-      ${shouldBackgroundBeGradient[path] ? "bg-gradient" : ""}
-    `}
-    >
-      <Navbar />
-      {children}
-      <Footer />
+    <Suspense fallback={<Loading />}>
+      <div
+        className={`${shouldBackgroundBeGradient[path] ? "bg-gradient" : ""}`}
+      >
+        <Navbar />
+        {children}
+        <Footer />
 
-      <ToastContainer
-        theme="dark"
-        position="bottom-right"
-        key="toast-container"
-      />
-    </div>
+        <ToastContainer
+          theme="dark"
+          position="bottom-right"
+          key="toast-container"
+        />
+      </div>
+    </Suspense>
   );
 }
