7c64e58079957050bfe7d8a788d0ec831d563279
chirigutraul <chirigutraul@gmail.com>
2023-06-28 20:49:48 +0300

    fix phone issue, removed await for client creation as we don't need it

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,19 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { getClientById, createClient } from "utils/hooks/requests/clients";
 
 export default NextAuth({
   secret: process.env.JWT_SECRET,
-  providers:[
+  providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-    })
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
   ],
   callbacks: {
     session: async ({ session, token, user }) => {
@@ -24,37 +24,33 @@ export default NextAuth({
       return session;
     },
     jwt: async ({ token, user, account, profile }) => {
-      user && (token.user = user)
-      account && (token.account = account)
-      profile && (token.profile = profile)
-      return token
+      user && (token.user = user);
+      account && (token.account = account);
+      profile && (token.profile = profile);
+      return token;
     },
-    async signIn (user) {
+    async signIn(user) {
       const sessionUser = user.user;
 
       const loggedClient = await getClientById(sessionUser.id);
 
-      if(!loggedClient.id){
+      if (!loggedClient.id) {
         const sessionUserFirstName = sessionUser.name.split(" ")[0];
         const sessionUserLastName = sessionUser.name.split(" ")[1];
 
         const newClientData = {
-          id:sessionUser.id,
+          id: sessionUser.id,
           firstName: sessionUserFirstName,
           lastName: sessionUserLastName,
           email: sessionUser.email,
           picture: sessionUser.image,
         };
 
+        createClient(newClientData);
+      }
 
-        const newClient = await createClient(newClientData);
-
-        user.user.phone = loggedClient?.phone || null;
-        return true
-    }
-
-    return true;
-
-    }
-  }
-})
+      user.user.phone = loggedClient?.phone || null;
+      return true;
+    },
+  },
+});
