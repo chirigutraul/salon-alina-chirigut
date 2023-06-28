603e13ff718a0ba2e3b3e91775a3ab9f105f19a1
chirigutraul <chirigutraul@gmail.com>
2023-06-28 21:00:05 +0300

    refactor get client by id endpoint, refactor profile & get client appointments services on frontend

--- pages/api/clients/[id].ts
+++ pages/api/clients/[id].ts
@@ -1,49 +1,53 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from 'prisma/client';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "prisma/client";
 
-import { ERROR_MESSAGES, SUCCESS_MESSAGES} from 'utils/constants';
+import { ERROR_MESSAGES, SUCCESS_MESSAGES } from "utils/constants";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { id } = req.query;
 
-  if(!id){
-    return res.status(400).json({message: ERROR_MESSAGES.ID_NOT_FOUND});
+  if (!id) {
+    return res.status(400).json({ message: ERROR_MESSAGES.ID_NOT_FOUND });
   }
 
   const { method } = req;
-  
-  if(method === 'GET'){
+
+  if (method === "GET") {
     const result = await prisma.client.findUnique({
       where: {
         id: id.toString(),
       },
-      include:{
-        appointments: {
-          include:{
-            service: true
-          }
-        }
-      }
     });
 
-    if(!result || !result.id) return res.status(404).json({message: ERROR_MESSAGES.USER_NOT_FOUND, status: 'error'});
+    if (!result || !result.id)
+      return res
+        .status(404)
+        .json({ message: ERROR_MESSAGES.USER_NOT_FOUND, status: "error" });
 
     return res.status(200).json(result);
   }
 
-  if(method === 'PATCH'){
+  if (method === "PATCH") {
     const { data } = req.body;
 
     const updateResult = await prisma.client.update({
-      where: { 
-        id: id.toString()
+      where: {
+        id: id.toString(),
       },
       data,
     });
 
-    if(!updateResult.id) return res.status(404).json({message: ERROR_MESSAGES.USER_NOT_FOUND, status: 'error'});
+    if (!updateResult.id)
+      return res
+        .status(404)
+        .json({ message: ERROR_MESSAGES.USER_NOT_FOUND, status: "error" });
 
-    return res.status(200).json({ message: SUCCESS_MESSAGES.NUMBER_ADDED, status: 'success'});
+    return res
+      .status(200)
+      .json({ message: SUCCESS_MESSAGES.NUMBER_ADDED, status: "success" });
   }
 }
--- utils/hooks/requests/clients/index.ts
+++ utils/hooks/requests/clients/index.ts
@@ -1,79 +1,43 @@
-import { Appointment } from "@prisma/client";
-import { IClient } from "types/DbEntitiesTypes"
-import { CLIENTS_API_URL } from "utils/constants"
+import { IClient } from "types/DbEntitiesTypes";
+import { CLIENTS_API_URL } from "utils/constants";
 
-export const getClientById = async (id:string) => {
+export const getClientById = async (id: string) => {
   const client = await fetch(`${CLIENTS_API_URL}/${id}`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
   const clientResponse = await client.json();
 
   return clientResponse;
-}
+};
 
-export const createClient = async (dto:IClient) => {
+export const createClient = async (dto: IClient) => {
   const result = await fetch(`${CLIENTS_API_URL}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      client: dto
+      client: dto,
     }),
-  })
+  });
 
   return result;
-}
+};
 
-export const updateClientById = async (id:string, dto:IClient) => {
- const updateResult = await fetch(`${CLIENTS_API_URL}/${id}`, {
-    method: 'PATCH',
+export const updateClientById = async (id: string, dto: IClient) => {
+  const updateResult = await fetch(`${CLIENTS_API_URL}/${id}`, {
+    method: "PATCH",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({data:dto}),
-  })
+    body: JSON.stringify({ data: dto }),
+  });
 
   const updateResultJson = await updateResult.json();
 
   return updateResultJson;
-}
-
-interface userProfileAppointments {
-  appointments: Appointment[];
-  closestAppointment: Appointment | null;
-}
-
-export async function getUserAppointments(userId: string): Promise<userProfileAppointments>{
-  const user = await fetch(`${CLIENTS_API_URL}/${userId}`);
-  const userJson = await user.json();
-
-  if(!userJson.appointments || !userJson.appointments.length) return {appointments: [], closestAppointment: null};
-
-  const sortedAppointments:Appointment[] = userJson.appointments.sort((a: Appointment, b: Appointment) => {
-   return new Date(a.date).getTime() -  new Date(b.date).getTime()
-  })
-
-  const dateToday = new Date();
-
-  let closestAppointment:Appointment | null = null;
-
-  for(const appointment of sortedAppointments){
-    if(new Date(appointment.date).getTime() > dateToday.getTime()){
-      closestAppointment = appointment;
-      break;
-    } 
-  }
-
-  const appointmentsAndSpotlight: userProfileAppointments = {
-    appointments: sortedAppointments,
-    closestAppointment: closestAppointment
-  }
-
-  return appointmentsAndSpotlight;
-}
-
+};
