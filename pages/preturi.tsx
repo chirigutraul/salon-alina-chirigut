1a9ef3c43522e6af87a28b78499924dfee05ede7
chirigutraul <chirigutraul@gmail.com>
2023-10-29 17:20:17 +0200

    Added loader in Preturi page

--- pages/preturi.tsx
+++ pages/preturi.tsx
@@ -1,19 +1,25 @@
 import { Service } from "@prisma/client";
+import { LoadingContext } from "components/Layout";
+import { useContext, useEffect, useState } from "react";
 import { servicesLabels } from "utils/constants";
 import { minutesToString } from "utils/helpers/format-hour";
 import { getServices } from "utils/hooks/requests/services";
 
-export const getServerSideProps: any = async () => {
-  const services = await getServices();
+export default function Preturi() {
+  const { setLoading, setDebouncedLoading } = useContext(LoadingContext);
+  const [servicesAndPrices, setServicesAndPrices] = useState<Service[]>();
 
-  return {
-    props: {
-      services,
-    },
+  const fetchServices = async () => {
+    setLoading(true);
+    const services = await getServices();
+    setServicesAndPrices(services);
+    setDebouncedLoading(false);
   };
-};
 
-export default function Preturi({ services }: { services: Service[] }) {
+  useEffect(() => {
+    fetchServices();
+  }, []);
+
   return (
     <section
       className={`relative h-full w-full flex flex-col items-center
@@ -29,6 +35,7 @@ export default function Preturi({ services }: { services: Service[] }) {
           className={`prices-table w-full table-auto 
            divide-solid whitespace-nowrap
            [&>*:nth-child(odd)]:bg-black-10
+           ${!servicesAndPrices && "h-[430px]"}
           `}
         >
           <thead>
@@ -37,10 +44,10 @@ export default function Preturi({ services }: { services: Service[] }) {
                 <h6>Serviciu</h6>
               </th>
               <th className={`sp-h sp-v`}>
-                <h6>Durata</h6>
+                <h6>Pret</h6>
               </th>
               <th className={`sp-h sp-v`}>
-                <h6>Pret</h6>
+                <h6>Durata</h6>
               </th>
             </tr>
           </thead>
@@ -49,21 +56,23 @@ export default function Preturi({ services }: { services: Service[] }) {
           [&>*:nth-child(even)]:bg-black-10
           `}
           >
-            {services.map((service) => {
-              return (
+            {servicesAndPrices?.length ? (
+              servicesAndPrices.map((service) => (
                 <tr key={service.id}>
                   <td className={`sp-h sp-v`}>
                     <h6>{servicesLabels.get(service.name)}</h6>
                   </td>
                   <td className={`sp-h sp-v`}>
-                    <h6>{minutesToString(service)}</h6>
-                  </td>
-                  <td className={`sp-h sp-v`}>
                     <h6>{service.price}lei</h6>
                   </td>
+                  <td className={`sp-h sp-v`}>
+                    <h6>{minutesToString(service)}</h6>
+                  </td>
                 </tr>
-              );
-            })}
+              ))
+            ) : (
+              <tr>Niciun serviciu gasit</tr>
+            )}
           </tbody>
         </table>
       </div>
